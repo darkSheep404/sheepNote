5b6384055bed94f804abbd2a970cf34c1d0d83ab
darksheep <sfa_freshman@qq.com>
2023-06-06 02:26:40 +0800

    [enhance]:调用编辑器 显示记录的代码

--- src/main/java/com/darksheep/sheepnote/toolWindow/NoteDetailPanel.java
+++ src/main/java/com/darksheep/sheepnote/toolWindow/NoteDetailPanel.java
@@ -2,25 +2,48 @@ package com.darksheep.sheepnote.toolWindow;
 
 import com.darksheep.sheepnote.data.NoteData;
 import com.intellij.ide.DataManager;
+import com.intellij.openapi.Disposable;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.EditorFactory;
+import com.intellij.openapi.editor.colors.EditorColorsManager;
+import com.intellij.openapi.editor.colors.EditorColorsScheme;
+import com.intellij.openapi.editor.highlighter.EditorHighlighter;
+import com.intellij.openapi.editor.highlighter.EditorHighlighterFactory;
+import com.intellij.openapi.editor.impl.EditorImpl;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.fileEditor.OpenFileDescriptor;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.fileTypes.FileTypeRegistry;
+import com.intellij.openapi.fileTypes.SyntaxHighlighter;
+import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.project.ProjectManagerListener;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.HyperlinkLabel;
 import com.intellij.ui.ScrollPaneFactory;
+import org.apache.commons.io.FilenameUtils;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.*;
 import java.text.SimpleDateFormat;
 
-public class NoteDetailPanel extends JPanel {
+import static java.awt.GridBagConstraints.REMAINDER;
+
+public class NoteDetailPanel extends JPanel{
     private JLabel titleLabel;
     private JLabel creationTimeLabel;
     private JLabel updateTimeLabel;
     private HyperlinkLabel filePathLabel;
-    private JTextArea codeTextArea;
+
+    private Editor codeEditor;
+
+    private Editor editorBase;
 
     private NoteData noteData;
 
@@ -68,6 +91,7 @@ public class NoteDetailPanel extends JPanel {
         });
         gbc.gridx = 0;
         gbc.gridy = 2;
+        gbc.gridwidth = REMAINDER;
         add(filePathLabel, gbc);
 
         // Customize GridBagConstraints for JTextArea
@@ -76,15 +100,77 @@ public class NoteDetailPanel extends JPanel {
         gbc.weightx = 1;
         gbc.weighty = 1;
         gbc.gridy = 4;
+        gbc.gridwidth = REMAINDER;
         // Reset insets for JTextArea
         gbc.insets = new Insets(0, 0, 0, 0);
 
         // Selected code
-        codeTextArea = new JTextArea();
-        codeTextArea.setEditable(false);
-        codeTextArea.setWrapStyleWord(true);
-        JScrollPane scrollPane = ScrollPaneFactory.createScrollPane(codeTextArea, true);
+        codeEditor = createCodeEditor();
+        JScrollPane scrollPane = ScrollPaneFactory.createScrollPane(codeEditor.getComponent(), true);
         add(scrollPane, gbc);
+    }
+
+    private Editor createCodeEditor() {
+        EditorFactory editorFactory = EditorFactory.getInstance();
+        Project currentProject = CommonDataKeys.PROJECT.getData(DataManager.getInstance().getDataContext(this));
+        Editor editor_base = editorFactory.createEditor(createDocument(), currentProject);
+        EditorImpl editor = (EditorImpl) editor_base; // Create the Editor
+        editor.getSettings().setFoldingOutlineShown(false);
+        editor.getSettings().setLineMarkerAreaShown(false);
+        editor.getSettings().setLineNumbersShown(true);
+        editor.getSettings().setIndentGuidesShown(false);
+
+        ProjectManager.getInstance().addProjectManagerListener(new ProjectManagerListener() {
+            @Override
+            public void projectClosing(@NotNull Project project) {
+                System.out.println("begin dispose");
+                System.out.println(codeEditor);
+                if(codeEditor!=null){
+                    EditorFactory.getInstance().releaseEditor(codeEditor);
+                   /* codeEditor = null;*/
+                }
+                ProjectManagerListener.super.projectClosing(project);
+            }
+        });
+      return editor;
+    }
+
+    private Document createDocument() {
+        Document document = EditorFactory.getInstance().createDocument(""); // Create an empty document
+        document.setReadOnly(false);
+        return document;
+    }
+    private void setEditorContent(String content) {
+       /* if(codeEditor == null){
+            codeEditor = createCodeEditor();
+        }*/
+        if (codeEditor != null) {
+            /**
+             * 设置 文本必须在ApplicationManager.getApplication().runWriteAction中设置 否则会报错
+             * ava.lang.Throwable: Assertion failed: Write access is allowed inside write-action only (see com.intellij.openapi.application.Application.runWriteAction())
+             * 	at com.intellij.openapi.diagnostic.Logger.assertTrue(Logger.java:218)
+             * 	at com.intellij.openapi.application.impl.ApplicationImpl.assertWriteAccessAllowed
+             */
+            ApplicationManager.getApplication().runWriteAction(() -> {
+                codeEditor.getDocument().setText(noteData.selectCode);
+
+                String fileExtension = FilenameUtils.getExtension(noteData.noteFilePath);
+                FileType fileType = FileTypeRegistry.getInstance().getFileTypeByExtension(fileExtension);
+                Project currentProject = CommonDataKeys.PROJECT.getData(DataManager.getInstance().getDataContext(this));
+
+                SyntaxHighlighter syntaxHighlighter = SyntaxHighlighterFactory.getSyntaxHighlighter(fileType, currentProject,null);
+                // Get editor colors scheme
+                EditorColorsScheme colorsScheme = EditorColorsManager.getInstance().getGlobalScheme();
+
+                // Create highlighter and apply it to the editor
+                EditorHighlighter highlighter = EditorHighlighterFactory.getInstance().createEditorHighlighter(syntaxHighlighter, colorsScheme);
+
+                highlighter.setText(codeEditor.getDocument().getImmutableCharSequence());
+                highlighter.setColorScheme(colorsScheme);
+                ((EditorImpl) codeEditor).setHighlighter(highlighter);
+            });
+
+        }
     }
 
     public void setNoteDetail(NoteData noteData) {
@@ -96,7 +182,8 @@ public class NoteDetailPanel extends JPanel {
         updateTimeLabel.setText("Update time: " + dateFormat.format(noteData.updateTime));
 
         filePathLabel.setHyperlinkText(noteData.noteFilePath + "#" + noteData.noteLineNumber);
-        codeTextArea.setText(noteData.selectCode);
+        setEditorContent(noteData.selectCode);
+        //codeTextArea.setText(noteData.selectCode);
     }
 }
 
